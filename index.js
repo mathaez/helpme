805c93015a6e9989fb3d0aa124fa80b1b951ad50
Mathis NICOLAS <mathis.nicolas2024@campus-eni.fr>
2025-07-01 11:14:29 +0200

    Premier commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT_NO || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('index', { message: 'Bienvenue sur HelpMe !' });
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
